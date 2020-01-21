20e268fb6a827b67869ed97994b089f3cb369c28
antoHero <veeqanto@gmail.com>
2020-01-21 11:51:04 +0100

    Install react and setup application

---
+++ redux_starter/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Posts from './components/Posts';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
